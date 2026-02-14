d65652d8935ea34155fa8f761ff080ab60aff2b9
ShivaNandhanaR <shivanandhanarathinamurthy@gmail.com>
2026-02-14 18:25:17 +0530

    Add files via upload

---
+++ program 3.py
@@ -0,0 +1,9 @@
+nums = [10, 20, 30, 40, 50]
+nums.append(60)      
+nums.insert(1, 15)  
+nums.remove(30)      
+nums.pop()           
+nums.sort()
+nums.reverse()
+new_list = nums.copy()
+nums.clear()
